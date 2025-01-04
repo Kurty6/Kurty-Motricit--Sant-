fa70bf8ceb78d60659061156a0c1d2846973a8aa
Kurty6 <Kurt04forest@gmail.com>
2025-01-04 17:27:02 +0100

    Update script.js
    Add google sheet's datas

--- script.js
+++ script.js
@@ -14,3 +14,29 @@ document.addEventListener("DOMContentLoaded", function() {
     })
     .catch(error => console.error('Erreur:', error));
 });
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/{1pa3wBesb1MuUYiaFOdzSbjaTKor7FcN8vp8fWb9cskA}/values/{Fiche de suivi 2025}?key={AIzaSyCFgsSrevhLarhUPfjOkKnhTw41jQAoVDA}')
+    .then(response => response.json())
+    .then(data => {
+        let container = document.getElementById('data-container');
+        data.values.forEach(row => {
+            let rowData = document.createElement('p');
+            rowData.textContent = row.join(', ');
+            container.appendChild(rowData);
+        });
+    })
+    .catch(error => console.error('Erreur:', error));
+});
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/{1pa3wBesb1MuUYiaFOdzSbjaTKor7FcN8vp8fWb9cskA}/values/{Valeurs}?key={AIzaSyCFgsSrevhLarhUPfjOkKnhTw41jQAoVDA}')
+    .then(response => response.json())
+    .then(data => {
+        let container = document.getElementById('data-container');
+        data.values.forEach(row => {
+            let rowData = document.createElement('p');
+            rowData.textContent = row.join(', ');
+            container.appendChild(rowData);
+        });
+    })
+    .catch(error => console.error('Erreur:', error));
+});
