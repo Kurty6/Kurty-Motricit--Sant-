44bd9d4013ded18ae8b14d872d5a10f7f5ae4de9
Kurty6 <Kurt04forest@gmail.com>
2025-01-04 17:10:44 +0100

    Update script.js
    Ajouter le script pour la connectivité avec Google Sheets

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
     console.log("Le site est chargé !");
 });
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('https://sheets.googleapis.com/v4/spreadsheets/{1pa3wBesb1MuUYiaFOdzSbjaTKor7FcN8vp8fWb9cskA}/values/{Fiche de renseignements}?key={AIzaSyCFgsSrevhLarhUPfjOkKnhTw41jQAoVDA}')
+    .then(response => response.json())
+    .then(data => {
+        let container = document.getElementById('data-container');
+        data.values.forEach(row => {
+            let rowData = document.createElement('p');
+            rowData.textContent = row.join(', ');
+            container.appendChild(rowData);
+        });
+    })
+    .catch(error => console.error('Erreur:', error));
+});
