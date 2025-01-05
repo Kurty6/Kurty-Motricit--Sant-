0f64d7a68f663a037e122e4c53ffa2d2cba60cd6
Kurty6 <Kurt04forest@gmail.com>
2025-01-05 23:35:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
+    function showAdhesionPage() {
+        alert('Nouvelle adhésion');
+    }
+
     console.log("Le site est prêt !");
 });
