51e90272cf46dd96dc81908f709f6bd6cdd71e36
Kurty6 <Kurt04forest@gmail.com>
2025-01-05 14:52:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,20 @@ document.addEventListener("DOMContentLoaded", function() {
     function showTabs() {
         document.querySelector('.tabs').style.display = 'flex';
         document.querySelector('.nouvelle-seance').style.display = 'block';
+        showBanner("Nouvelle adhésion");
+    }
+
+    function showBanner(text) {
+        const banner = document.getElementById('banner');
+        banner.textContent = text;
+        banner.style.display = 'block';
     }
 
     function handleSearchAndRedirect(query) {
         // Logique pour rechercher et rediriger vers la page de l'adhérant
         // Exemple : Si le nom correspond à un adhérant
         if (query === "Nom de l'adhérant") {
+            showBanner(query);
             window.location.href = 'memberPage.html';
         }
     }
